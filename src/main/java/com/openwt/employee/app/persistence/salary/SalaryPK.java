786b9aa695e0dda6e1ff38443362c49c106e06bc
Luc Kaing <lkaing@openwt.com>
2022-12-06 11:54:48 +0100

    remove useless lombok annotations

--- src/main/java/com/openwt/employee/app/persistence/salary/SalaryPK.java
+++ src/main/java/com/openwt/employee/app/persistence/salary/SalaryPK.java
@@ -1,13 +1,13 @@
 package com.openwt.employee.app.persistence.salary;
 
 import jakarta.persistence.Embeddable;
-import lombok.Data;
+import lombok.Getter;
 
 
 import java.io.Serializable;
 import java.util.Date;
 @Embeddable
-@Data
+@Getter
 public class SalaryPK implements Serializable {
     private Long empNo;
     private Date fromDate;
