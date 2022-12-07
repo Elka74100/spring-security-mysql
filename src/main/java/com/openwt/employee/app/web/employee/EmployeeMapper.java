b2f8dd096f9a5275903dde00bb9ca47fee6d5a7d
Luc Kaing <lkaing@openwt.com>
2022-12-07 10:16:51 +0100

    fix mapper

--- src/main/java/com/openwt/employee/app/web/employee/EmployeeMapper.java
+++ src/main/java/com/openwt/employee/app/web/employee/EmployeeMapper.java
@@ -1,9 +1,12 @@
 package com.openwt.employee.app.web.employee;
 
 import com.openwt.employee.app.persistence.employee.Employee;
+import com.openwt.employee.app.persistence.salary.Salary;
 import com.openwt.employee.app.web.employee.dto.EmployeeDetailedDto;
 import com.openwt.employee.app.web.employee.dto.EmployeeDto;
+import com.openwt.employee.app.web.employee.dto.SalaryDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -11,4 +14,6 @@ import java.util.List;
 public interface EmployeeMapper {
     List<EmployeeDto> toDto(List<Employee> employee);
     EmployeeDetailedDto toDto(Employee employee);
+    @Mapping(target="fromDate", source="salaryPK.fromDate")
+    SalaryDto toDto(Salary salary);
 }
