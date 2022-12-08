a6f8b4831b3a773bd0b11fc37735c57e957b5cd7
Luc Kaing <lkaing@openwt.com>
2022-12-08 09:39:20 +0100

    clean code

--- src/main/java/com/openwt/employee/app/persistence/employee/EmployeeRepository.java
+++ src/main/java/com/openwt/employee/app/persistence/employee/EmployeeRepository.java
@@ -2,15 +2,14 @@ package com.openwt.employee.app.persistence.employee;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
     Employee findByEmpNo(Long empNo);
     @Query(
-            value = "SELECT AVG(salaries.salary) FROM salaries INNER JOIN dept_emp ON salaries.emp_no = dept_emp.emp_no WHERE dept_no = :#{#deptNo}",
+            value = "SELECT AVG(salaries.salary) FROM salaries INNER JOIN dept_emp ON salaries.emp_no = dept_emp.emp_no WHERE dept_no = :deptNo",
             nativeQuery = true
     )
-    Double findAverageSalaryPerDepartment(@Param("deptNo") String deptNo);
+    Double findAverageSalaryPerDepartment(String deptNo);
 }
