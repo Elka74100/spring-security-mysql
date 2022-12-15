6e58ecc9b16db70667f362ef5c710ece179e4dc4
Luc Kaing <lkaing@openwt.com>
2022-12-15 09:42:22 +0100

    stub user service

--- src/main/java/com/openwt/employee/web/user/dto/AuthenticationRequestDto.java
+++ src/main/java/com/openwt/employee/web/user/dto/AuthenticationRequestDto.java
@@ -1,10 +1,12 @@
 package com.openwt.employee.web.user.dto;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode
 public class AuthenticationRequestDto {
     private String email;
     private String password;
--- src/test/java/com/openwt/employee/web/user/UserControllerTest.java
+++ src/test/java/com/openwt/employee/web/user/UserControllerTest.java
@@ -2,29 +2,29 @@ package com.openwt.employee.web.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import com.openwt.employee.service.user.UserService;
 import com.openwt.employee.web.user.dto.AuthenticationRequestDto;
 import com.openwt.employee.web.user.dto.RegistrationRequestDto;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class UserControllerTest {
@@ -32,7 +32,12 @@ class UserControllerTest {
     @Autowired
     private WebApplicationContext webApplicationContext;
     private MockMvc mvc;
+
     private final ObjectMapper objectMapper = new ObjectMapper();
+    private static final String authToken = "authToken";
+
+    @MockBean
+    private UserService userService;
 
     @BeforeEach
     void setup() {
@@ -43,10 +48,12 @@ class UserControllerTest {
     void shouldRegisterSuccessfully() throws Exception {
 
         final RegistrationRequestDto request = new RegistrationRequestDto();
-        request.setEmail("lkaing@openwt.com");
+        request.setEmail("my_email@blabla.com");
         request.setPassword("password");
         request.setRoles("ROLE_ADMIN");
         request.setActive(true);
+
+        when(userService.register(request)).thenReturn(authToken);
 
         MvcResult result =  mvc.perform(
                 post("/api/auth/register")
@@ -56,15 +63,18 @@ class UserControllerTest {
                 .andExpect(status().isOk())
                 .andReturn();
 
-        assertNotNull(result);
+        assertEquals(authToken , result.getResponse().getContentAsString());
+        verify(userService).register(request);
     }
 
     @Test
     void shouldAuthenticateSuccessfully() throws Exception {
 
         final AuthenticationRequestDto request = new AuthenticationRequestDto();
-        request.setEmail("lkaing@openwt.com");
+        request.setEmail("my_email@blabla.com");
         request.setPassword("password");
+
+        when(userService.authenticate(request)).thenReturn(authToken);
 
         MvcResult result =  mvc.perform(
                         post("/api/auth/authenticate")
@@ -74,7 +84,8 @@ class UserControllerTest {
                 .andExpect(status().isOk())
                 .andReturn();
 
-        assertNotNull(result);
+        assertEquals(authToken , result.getResponse().getContentAsString());
+        verify(userService).authenticate(request);
     }
 
 }
